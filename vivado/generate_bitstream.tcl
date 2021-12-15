331d74fb005e4197da1e27abaec7a213eb3413cd
Howard Lau <howardlau1999@hotmail.com>
2021-12-16 00:44:20 +0800

    vivado: do not create project if it exists

--- vivado/generate_bitstream.tcl
+++ vivado/generate_bitstream.tcl
@@ -1,6 +1,23 @@
 # Only tested on Vivado 2020.1 on Windows 10
-create_project -force riscv-basys3 riscv-basys3 -part xc7a35tcpg236-1
-add_files -norecurse {../verilog/BCD2Segments.v ../verilog/OnboardDigitDisplay.v ../verilog/Top.v ../verilog/SegmentMux.v}
+
+# set variables
+set project_dir riscv-basys3
+set project_name riscv-basys3
+set part xc7a35tcpg236-1
+set sources {../verilog/BCD2Segments.v ../verilog/OnboardDigitDisplay.v ../verilog/Top.v ../verilog/SegmentMux.v}
+
+# open the project. will create one if it doesn't exist 
+if {[file exist $project_dir]} {
+    # check that it's a directory
+    if {! [file isdirectory $project_dir]} {
+        puts "$project_dir exists, but it's a file"
+    }
+    open_project $project_dir/$project_name.xpr -part $part 
+} else {
+    create_project riscv-basys3 riscv-basys3 -part $part
+}
+
+add_files -norecurse $sources
 update_compile_order -fileset sources_1
 add_files -fileset constrs_1 -norecurse ./basys3.xdc
 launch_runs impl_1 -to_step write_bitstream -jobs 4
