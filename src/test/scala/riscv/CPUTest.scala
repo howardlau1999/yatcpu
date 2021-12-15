5658df610577cbb043b2968349733b31d949dbb4
Howard Lau <howardlau1999@hotmail.com>
2021-12-15 21:36:03 +0800

    *: make memory smaller, cleanup test code and add C source code

--- src/test/scala/riscv/CPUTest.scala
+++ src/test/scala/riscv/CPUTest.scala
@@ -14,7 +14,8 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
       val pc = c.io.instruction_address.peek().litValue() >> 2
       if (pc >= instructions.length) {
         if (!warned) {
-          println(f"Out of range at cycle $i%d pc=0x$i%x")
+          println(f"PC is out of range at cycle $i%d PC=0x${pc * 4}%x. This means the IF is trying to fetch code " +
+            f"after the program has ended, which is normal.")
           warned = true
         }
         c.io.instruction.poke(0x00000013.U)
@@ -64,12 +65,6 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
           0x00f00293L.U,
           0x00500223L.U,
           0x00400303L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
         )
         run_instructions(instructions, c, 7)
         c.io.debug_read_address.poke(6.U)
@@ -101,10 +96,8 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
           0x00130313L.U,
           0xfe63dce3L.U,
           0x0000006fL.U,
-          0x00000013L.U,
-          0x00000013L.U,
         )
-        run_instructions(instructions, c, 6000)
+        run_instructions(instructions, c, 1000)
         c.io.debug_read_address.poke(5.U)
         c.io.debug_read_data.expect(5050.U)
       }
@@ -112,62 +105,26 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
 
     "should execute recursive fibonacci" in {
       test(new CPU) { c =>
+        /*
+          int fib(int a) {
+                  if (a == 1 || a == 2) return 1;
+                  return fib(a - 1) + fib(a - 2);
+          }
+
+          int main() {
+                  *(int *)(4) = fib(10);
+          }
+         */
         val instructions: Array[UInt] = Array(
-          0x3fc00113L.U,
-          0x094000efL.U,
-          0x0000006fL.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0xfe010113L.U,
-          0x00112e23L.U,
-          0x00812c23L.U,
-          0x00912a23L.U,
-          0x02010413L.U,
-          0xfea42623L.U,
-          0xfec42703L.U,
-          0x00100793L.U,
-          0x00f70863L.U,
-          0xfec42703L.U,
-          0x00200793L.U,
-          0x00f71663L.U,
-          0x00100793L.U,
-          0x0300006fL.U,
-          0xfec42783L.U,
-          0xfff78793L.U,
-          0x00078513L.U,
-          0xfbdff0efL.U,
-          0x00050493L.U,
-          0xfec42783L.U,
-          0xffe78793L.U,
-          0x00078513L.U,
-          0xfa9ff0efL.U,
-          0x00050793L.U,
-          0x00f487b3L.U,
-          0x00078513L.U,
-          0x01c12083L.U,
-          0x01812403L.U,
-          0x01412483L.U,
-          0x02010113L.U,
-          0x00008067L.U,
-          0xff010113L.U,
-          0x00112623L.U,
-          0x00812423L.U,
-          0x00912223L.U,
-          0x01010413L.U,
-          0x00400493L.U,
-          0x00a00513L.U,
-          0xf69ff0efL.U,
-          0x00050793L.U,
-          0x00f4a023L.U,
-          0x00000793L.U,
-          0x00078513L.U,
-          0x00c12083L.U,
-          0x00812403L.U,
-          0x00412483L.U,
-          0x01010113L.U,
-          0x00008067L.U,
+          0x3fc00113L.U, 0x084000efL.U, 0x0000006fL.U, 0xfe010113L.U, 0x00112e23L.U, 0x00812c23L.U,
+          0x00912a23L.U, 0x02010413L.U, 0xfea42623L.U, 0xfec42703L.U, 0x00100793L.U, 0x00f70863L.U,
+          0xfec42703L.U, 0x00200793L.U, 0x00f71663L.U, 0x00100793L.U, 0x0300006fL.U, 0xfec42783L.U,
+          0xfff78793L.U, 0x00078513L.U, 0xfbdff0efL.U, 0x00050493L.U, 0xfec42783L.U, 0xffe78793L.U,
+          0x00078513L.U, 0xfa9ff0efL.U, 0x00050793L.U, 0x00f487b3L.U, 0x00078513L.U, 0x01c12083L.U,
+          0x01812403L.U, 0x01412483L.U, 0x02010113L.U, 0x00008067L.U, 0xff010113L.U, 0x00112623L.U,
+          0x00812423L.U, 0x00912223L.U, 0x01010413L.U, 0x00400493L.U, 0x00a00513L.U, 0xf69ff0efL.U,
+          0x00050793L.U, 0x00f4a023L.U, 0x00000793L.U, 0x00078513L.U, 0x00c12083L.U, 0x00812403L.U,
+          0x00412483L.U, 0x01010113L.U, 0x00008067L.U,
         )
         run_instructions(instructions, c, 3500)
         c.io.debug_mem_read_address.poke(4.U)
@@ -176,163 +133,67 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
     }
 
     "should quick sort 10 numbers" in {
+      /*
+        void quicksort(int *arr, int l, int r) {
+                if (l >= r) return;
+                int pivot = arr[l];
+                int i = l, j = r;
+                while (i < j) {
+                        while(arr[j] >= pivot && i < j) --j;
+                        arr[i] = arr[j];
+                        while(arr[i] < pivot && i < j) ++i;
+                        arr[j] = arr[i];
+                }
+                arr[i] = pivot;
+                quicksort(arr, l, i - 1);
+                quicksort(arr, i + 1, r);
+        }
+
+        int main() {
+                int nums[10];
+                nums[0] = 6;
+                nums[1] = 2;
+                nums[2] = 4;
+                nums[3] = 5;
+                nums[4] = 3;
+                nums[5] = 1;
+                nums[6] = 0;
+                nums[7] = 9;
+                nums[8] = 7;
+                nums[9] = 8;
+
+
+                quicksort(nums, 0, 9);
+
+                for (int i = 1; i <= 10; ++i) {
+                        *(int *)(i * 4) = nums[i - 1];
+                }
+        }
+       */
       test(new CPU) { c =>
         val instructions: Array[UInt] = Array(
-          0x00001137L.U,
-          0xffc10113L.U,
-          0x194000efL.U,
-          0x0000006fL.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0x00000013L.U,
-          0xfd010113L.U,
-          0x02112623L.U,
-          0x02812423L.U,
-          0x03010413L.U,
-          0xfca42e23L.U,
-          0xfcb42c23L.U,
-          0xfcc42a23L.U,
-          0xfd842703L.U,
-          0xfd442783L.U,
-          0x14f75263L.U,
-          0xfd842783L.U,
-          0x00279793L.U,
-          0xfdc42703L.U,
-          0x00f707b3L.U,
-          0x0007a783L.U,
-          0xfef42223L.U,
-          0xfd842783L.U,
-          0xfef42623L.U,
-          0xfd442783L.U,
-          0xfef42423L.U,
-          0x0c00006fL.U,
-          0xfe842783L.U,
-          0xfff78793L.U,
-          0xfef42423L.U,
-          0xfe842783L.U,
-          0x00279793L.U,
-          0xfdc42703L.U,
-          0x00f707b3L.U,
-          0x0007a783L.U,
-          0xfe442703L.U,
-          0x00e7c863L.U,
-          0xfec42703L.U,
-          0xfe842783L.U,
-          0xfcf748e3L.U,
-          0xfe842783L.U,
-          0x00279793L.U,
-          0xfdc42703L.U,
-          0x00f70733L.U,
-          0xfec42783L.U,
-          0x00279793L.U,
-          0xfdc42683L.U,
-          0x00f687b3L.U,
-          0x00072703L.U,
-          0x00e7a023L.U,
-          0x0100006fL.U,
-          0xfec42783L.U,
-          0x00178793L.U,
-          0xfef42623L.U,
-          0xfec42783L.U,
-          0x00279793L.U,
-          0xfdc42703L.U,
-          0x00f707b3L.U,
-          0x0007a783L.U,
-          0xfe442703L.U,
-          0x00e7d863L.U,
-          0xfec42703L.U,
-          0xfe842783L.U,
-          0xfcf748e3L.U,
-          0xfec42783L.U,
-          0x00279793L.U,
-          0xfdc42703L.U,
-          0x00f70733L.U,
-          0xfe842783L.U,
-          0x00279793L.U,
-          0xfdc42683L.U,
-          0x00f687b3L.U,
-          0x00072703L.U,
-          0x00e7a023L.U,
-          0xfec42703L.U,
-          0xfe842783L.U,
-          0xf4f744e3L.U,
-          0xfec42783L.U,
-          0x00279793L.U,
-          0xfdc42703L.U,
-          0x00f707b3L.U,
-          0xfe442703L.U,
-          0x00e7a023L.U,
-          0xfec42783L.U,
-          0xfff78793L.U,
-          0x00078613L.U,
-          0xfd842583L.U,
-          0xfdc42503L.U,
-          0xeb9ff0efL.U,
-          0xfec42783L.U,
-          0x00178793L.U,
-          0xfd442603L.U,
-          0x00078593L.U,
-          0xfdc42503L.U,
-          0xea1ff0efL.U,
-          0x0080006fL.U,
-          0x00000013L.U,
-          0x02c12083L.U,
-          0x02812403L.U,
-          0x03010113L.U,
-          0x00008067L.U,
-          0xfc010113L.U,
-          0x02112e23L.U,
-          0x02812c23L.U,
-          0x04010413L.U,
-          0x00600793L.U,
-          0xfcf42223L.U,
-          0x00200793L.U,
-          0xfcf42423L.U,
-          0x00400793L.U,
-          0xfcf42623L.U,
-          0x00500793L.U,
-          0xfcf42823L.U,
-          0x00300793L.U,
-          0xfcf42a23L.U,
-          0x00100793L.U,
-          0xfcf42c23L.U,
-          0xfc042e23L.U,
-          0x00900793L.U,
-          0xfef42023L.U,
-          0x00700793L.U,
-          0xfef42223L.U,
-          0x00800793L.U,
-          0xfef42423L.U,
-          0xfc440793L.U,
-          0x00900613L.U,
-          0x00000593L.U,
-          0x00078513L.U,
-          0xe19ff0efL.U,
-          0x00100793L.U,
-          0xfef42623L.U,
-          0x0340006fL.U,
-          0xfec42783L.U,
-          0xfff78793L.U,
-          0xfec42703L.U,
-          0x00271713L.U,
-          0x00279793L.U,
-          0xff040693L.U,
-          0x00f687b3L.U,
-          0xfd47a783L.U,
-          0x00f72023L.U,
-          0xfec42783L.U,
-          0x00178793L.U,
-          0xfef42623L.U,
-          0xfec42703L.U,
-          0x00a00793L.U,
-          0xfce7d4e3L.U,
-          0x00000793L.U,
-          0x00078513L.U,
-          0x03c12083L.U,
-          0x03812403L.U,
-          0x04010113L.U,
-          0x00008067L.U,
+          0x3fc00113L.U, 0x184000efL.U, 0x0000006fL.U, 0xfd010113L.U, 0x02112623L.U, 0x02812423L.U, 0x03010413L.U,
+          0xfca42e23L.U, 0xfcb42c23L.U, 0xfcc42a23L.U, 0xfd842703L.U, 0xfd442783L.U, 0x14f75263L.U, 0xfd842783L.U,
+          0x00279793L.U, 0xfdc42703L.U, 0x00f707b3L.U, 0x0007a783L.U, 0xfef42223L.U, 0xfd842783L.U, 0xfef42623L.U,
+          0xfd442783L.U, 0xfef42423L.U, 0x0c00006fL.U, 0xfe842783L.U, 0xfff78793L.U, 0xfef42423L.U, 0xfe842783L.U,
+          0x00279793L.U, 0xfdc42703L.U, 0x00f707b3L.U, 0x0007a783L.U, 0xfe442703L.U, 0x00e7c863L.U, 0xfec42703L.U,
+          0xfe842783L.U, 0xfcf748e3L.U, 0xfe842783L.U, 0x00279793L.U, 0xfdc42703L.U, 0x00f70733L.U, 0xfec42783L.U,
+          0x00279793L.U, 0xfdc42683L.U, 0x00f687b3L.U, 0x00072703L.U, 0x00e7a023L.U, 0x0100006fL.U, 0xfec42783L.U,
+          0x00178793L.U, 0xfef42623L.U, 0xfec42783L.U, 0x00279793L.U, 0xfdc42703L.U, 0x00f707b3L.U, 0x0007a783L.U,
+          0xfe442703L.U, 0x00e7d863L.U, 0xfec42703L.U, 0xfe842783L.U, 0xfcf748e3L.U, 0xfec42783L.U, 0x00279793L.U,
+          0xfdc42703L.U, 0x00f70733L.U, 0xfe842783L.U, 0x00279793L.U, 0xfdc42683L.U, 0x00f687b3L.U, 0x00072703L.U,
+          0x00e7a023L.U, 0xfec42703L.U, 0xfe842783L.U, 0xf4f744e3L.U, 0xfec42783L.U, 0x00279793L.U, 0xfdc42703L.U,
+          0x00f707b3L.U, 0xfe442703L.U, 0x00e7a023L.U, 0xfec42783L.U, 0xfff78793L.U, 0x00078613L.U, 0xfd842583L.U,
+          0xfdc42503L.U, 0xeb9ff0efL.U, 0xfec42783L.U, 0x00178793L.U, 0xfd442603L.U, 0x00078593L.U, 0xfdc42503L.U,
+          0xea1ff0efL.U, 0x0080006fL.U, 0x00000013L.U, 0x02c12083L.U, 0x02812403L.U, 0x03010113L.U, 0x00008067L.U,
+          0xfc010113L.U, 0x02112e23L.U, 0x02812c23L.U, 0x04010413L.U, 0x00600793L.U, 0xfcf42223L.U, 0x00200793L.U,
+          0xfcf42423L.U, 0x00400793L.U, 0xfcf42623L.U, 0x00500793L.U, 0xfcf42823L.U, 0x00300793L.U, 0xfcf42a23L.U,
+          0x00100793L.U, 0xfcf42c23L.U, 0xfc042e23L.U, 0x00900793L.U, 0xfef42023L.U, 0x00700793L.U, 0xfef42223L.U,
+          0x00800793L.U, 0xfef42423L.U, 0xfc440793L.U, 0x00900613L.U, 0x00000593L.U, 0x00078513L.U, 0xe19ff0efL.U,
+          0x00100793L.U, 0xfef42623L.U, 0x0340006fL.U, 0xfec42783L.U, 0xfff78793L.U, 0xfec42703L.U, 0x00271713L.U,
+          0x00279793L.U, 0xff040693L.U, 0x00f687b3L.U, 0xfd47a783L.U, 0x00f72023L.U, 0xfec42783L.U, 0x00178793L.U,
+          0xfef42623L.U, 0xfec42703L.U, 0x00a00793L.U, 0xfce7d4e3L.U, 0x00000793L.U, 0x00078513L.U, 0x03c12083L.U,
+          0x03812403L.U, 0x04010113L.U, 0x00008067L.U
         )
         run_instructions(instructions, c, 1550)
         for (i <- 1 to 10) {
