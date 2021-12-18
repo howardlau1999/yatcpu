50389467b2c67fa5046b6a07d73ff83c686584ed
wu-kan <i@wu-kan.cn>
2021-12-18 21:27:23 +0800

    optVGADisplay.scala: use MuxLookup & Mux

--- src/main/scala/riscv/VGADisplay.scala
+++ src/main/scala/riscv/VGADisplay.scala
@@ -21,22 +21,18 @@ class VGADisplay extends Module {
   io.char_mem_address := glyph_index + 1024.U
   val offset = glyph_index % 4.U
   val ch = Wire(UInt(8.W))
-  when(offset === 0.U) {
-    ch := io.char_mem_data(7, 0).asUInt()
-  }.elsewhen(offset === 1.U) {
-    ch := io.char_mem_data(15, 8).asUInt()
-  }.elsewhen(offset === 2.U) {
-    ch := io.char_mem_data(23, 16).asUInt()
-  }.elsewhen(offset === 3.U) {
-    ch := io.char_mem_data(31, 24).asUInt()
-  }.otherwise {
-    ch := 0.U
-  }
 
-  io.glyph_rom_index := 0.U
-  when(ch >= 32.U) {
-    io.glyph_rom_index := ch - 31.U
-  }
+  ch := MuxLookup(
+    offset,
+    0.U,
+    Array(
+      0.U -> io.char_mem_data(7, 0).asUInt(),
+      1.U -> io.char_mem_data(15, 8).asUInt(),
+      2.U -> io.char_mem_data(23, 16).asUInt(),
+      3.U -> io.char_mem_data(31, 24).asUInt()
+    )
+  )
+  io.glyph_rom_index := Mux(ch >= 32.U, ch - 31.U, 0.U)
 
   io.glyph_x := io.screen_x % GlyphInfo.glyphWidth.U
   io.glyph_y := io.screen_y % GlyphInfo.glyphHeight.U
