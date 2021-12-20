3c0b083814b55cd485f2f4751b2896aad5fc72ec
Howard Lau <howardlau1999@hotmail.com>
2021-12-21 00:31:04 +0800

    test: fix ProgramCounterTest

--- src/test/scala/riscv/ProgramCounterTest.scala
+++ src/test/scala/riscv/ProgramCounterTest.scala
@@ -6,17 +6,17 @@ import org.scalatest._
 
 class ProgramCounterTest extends FreeSpec with ChiselScalatestTester {
   "PC should " - {
-    "be reset to 0 when reset is true" in {
+    "be reset to entry address when reset is true" in {
       test(new ProgramCounter) { c =>
         c.io.jump_enable.poke(false.B)
         c.io.jump_address.poke(0.U)
         c.io.hold_flag.poke(0.U)
         c.clock.step()
-        c.io.pc.expect(0x4L.U)
+        c.io.pc.expect((ProgramCounter.EntryAddress.litValue() + 0x4).U)
         c.reset.poke(true.B)
         c.clock.step()
         c.reset.poke(false.B)
-        c.io.pc.expect(0x0L.U)
+        c.io.pc.expect(ProgramCounter.EntryAddress)
       }
     }
 
@@ -35,11 +35,11 @@ class ProgramCounterTest extends FreeSpec with ChiselScalatestTester {
         c.io.jump_enable.poke(false.B)
         c.io.hold_flag.poke(0.U)
         c.clock.step()
-        c.io.pc.expect(0x4L.U)
+        c.io.pc.expect((ProgramCounter.EntryAddress.litValue() + 0x4).U)
 
         c.io.hold_flag.poke(1.U)
         c.clock.step()
-        c.io.pc.expect(0x4L.U)
+        c.io.pc.expect((ProgramCounter.EntryAddress.litValue() + 0x4).U)
       }
     }
   }
