79d552c2a8d5aff5ef48fd3b87e9f60d71af2654
Howard Lau <howardlau1999@hotmail.com>
2021-12-21 16:31:08 +0800

    test: add test back

--- src/test/scala/riscv/CPUTest.scala
+++ src/test/scala/riscv/CPUTest.scala
@@ -1,12 +1,11 @@
 package riscv
 
-import board.basys3.Top
 import chisel3._
-import chisel3.stage.{ChiselGeneratorAnnotation, ChiselStage}
 import chisel3.tester._
 import org.scalatest._
 
 import java.nio.{ByteBuffer, ByteOrder}
+
 class CPUTest extends FreeSpec with ChiselScalatestTester {
   class TestMemory(capacity: Int = 8192, asmBin: String) extends Module {
     val io = IO(new Bundle {
@@ -26,15 +25,15 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
     val read_data = Reg(UInt(32.W))
     val inst_data = Reg(UInt(32.W))
     val dbg_data = Reg(UInt(32.W))
-    read_data := mem(io.read_address)
-    inst_data := mem(io.instruction_read_address)
-    dbg_data := mem(io.debug_read_address)
+    read_data := mem(io.read_address / 4.U)
+    inst_data := mem((io.instruction_read_address - 0x1000.U + 1024.U) / 4.U)
+    dbg_data := mem(io.debug_read_address / 4.U)
     io.read_data := read_data
     io.instruction_read_data := inst_data
     io.debug_read_data := dbg_data
 
     when(io.write_enable) {
-      mem(io.write_address) := io.write_data
+      mem(io.write_address / 4.U) := io.write_data
     }
 
     def loadAsmBinary(filename: String) = {
@@ -50,7 +49,7 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
       instructions = instructions :+ BigInt(0x00000013L)
       instructions = instructions :+ BigInt(0x00000013L)
       instructions = instructions :+ BigInt(0x00000013L)
-      VecInit((Seq.fill(1024)(BigInt(0)) ++ instructions ++ Seq.fill(2048)(BigInt(0))).map(inst => inst.U(32.W)))
+      VecInit((Seq.fill(256)(BigInt(0)) ++ instructions).map(inst => inst.U(32.W)))
     }
   }
 
@@ -61,10 +60,12 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
       val regs_debug_read_data = Output(UInt(32.W))
       val mem_debug_read_data = Output(UInt(32.W))
 
+      val a = Output(UInt(32.W))
       val interrupt = Input(UInt(32.W))
     })
     val mem = Module(new TestMemory(8192, exeFilename))
     val cpu = Module(new CPU)
+    io.a := cpu.io.instruction_read_address
     mem.io.debug_read_address := io.mem_debug_read_address
     mem.io.instruction_read_address := cpu.io.instruction_read_address
     cpu.io.instruction_read_data := mem.io.instruction_read_data
@@ -81,8 +82,32 @@ class CPUTest extends FreeSpec with ChiselScalatestTester {
   }
 
   "CPU " - {
-    "should compile to verilog" in {
-      (new ChiselStage).execute(Array("-X", "verilog", "-td", "verilog"), Seq(ChiselGeneratorAnnotation(() => new Top)))
+    "should calculate recursive fibonacci" in {
+      test(new TestTopModule("fibonacci.asmbin")) { c =>
+        c.io.interrupt.poke(0.U)
+        for (i <- 1 to 5000) {
+          c.clock.step()
+          c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+        }
+        c.io.mem_debug_read_address.poke(4.U)
+        c.clock.step()
+        c.io.mem_debug_read_data.expect(55.U)
+      }
+    }
+
+    "should quicksort 10 numbers" in {
+      test(new TestTopModule("quicksort.asmbin")) { c =>
+        c.io.interrupt.poke(0.U)
+        for (i <- 1 to 3000) {
+          c.clock.step()
+          c.io.mem_debug_read_address.poke((i * 4).U) // Avoid timeout
+        }
+        for (i <- 1 to 10) {
+          c.io.mem_debug_read_address.poke((4 * i).U)
+          c.clock.step()
+          c.io.mem_debug_read_data.expect((i - 1).U)
+        }
+      }
     }
   }
 }
