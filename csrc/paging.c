321dc5ca70c8336f6edbd88434bf9d4d9de2d8cb
hrpccs <2634367909@qq.com>
2022-05-31 23:47:31 +0800

    mmu module

--- csrc/paging.c
+++ csrc/paging.c
@@ -61,22 +61,14 @@ int wk_mul(int a, int b) {
 }
 
 uint32 pm[8]; //板子上有32kb内存，八个页
-uint32 timercount=0;
+int timerflag=0;
 
 char info[][16] = {
     "page fault      ",
     "start paging    ",
     "timer interrupt ",
-    "out of mem      ",
 };
 
-void putch_at(int x, int y, unsigned char ch) {
-	VRAM[wk_mul(y,SCREEN_COLS) + x] = ch;
-}
-
-void vputch_at(int x, int y, unsigned char ch) {
-	VA_VRAM[wk_mul(y,SCREEN_COLS) + x] = ch;
-}
 
 int alloc(){
     int index = 0;
@@ -91,31 +83,24 @@ int alloc(){
     return index == max ? -1 : index;
 }
 
-void map(pagetable_t pgtbl,uint32 va,uint32 pa,int perm){
+int map(pagetable_t pgtbl,uint32 va,uint32 pa,int perm){
     int t;
-    if((pgtbl[PX(1,va)] & PTE_V )!= PTE_V){ //前缀不存在
-        t=alloc(); //申请一个页给前缀页表
+    if((pgtbl[PX(1,va)] | PTE_V )!= 1){
+        t=alloc();
         if(t>=0){
             pgtbl[PX(1,va)] = PA2PTE(t<<12) | PTE_V;
         }else{
-            for(int i=0;i<16;i++){
-                putch_at(50+i,0,info[3][i]);
-            }
-            while(1);
+            return -1;
         }
     }
-    pagetable_t n = (void*)PTE2PA(pgtbl[PX(1,va)]);
-    n[PX(0,va)] = PA2PTE(pa) | perm | PTE_V;
+    t=PTE2PA(pgtbl[PX(1,va)]);
+    ((pagetable_t) t)[PX(0,va)] = PA2PTE(pa) | perm | PTE_V;
+    return 0;
 }
 
 void kvminit(){
     pagetable_t pgtbl = (void*)PAGEDIR_BASE;
     pm[PAGEDIR_BASE >> 12] = 1;
-    pm[0]=1;
-    pm[1]=1;
-    pm[2]=1;
-    pm[3]=1;
-    pm[4]=1;
     //create pte mmap for text
     map(pgtbl,PAGEDIR_BASE,PAGEDIR_BASE,PTE_R | PTE_W);
     map(pgtbl,0x0,0x0, PTE_W | PTE_R ); //kernel stack
@@ -123,10 +108,15 @@ void kvminit(){
     map(pgtbl,0x2000,0x2000, PTE_W | PTE_R | PTE_X ); //
     map(pgtbl,0x3000,0x3000, PTE_W | PTE_R | PTE_X ); //
     map(pgtbl,0x4000,0x4000, PTE_W | PTE_R | PTE_X ); //
-    map(pgtbl,VA_VRAM_BASE,VRAM_BASE, PTE_W | PTE_R ); //
-    map(pgtbl,VA_VRAM_BASE + PGSIZE,VRAM_BASE + PGSIZE, PTE_W | PTE_R);
-    map(pgtbl,VA_UART_BASE,UART_BASE, PTE_W | PTE_R ); //
-    map(pgtbl,VA_TIMER_BASE,TIMER_BASE, PTE_W | PTE_R ); //
+    map(pgtbl,VRAM_BASE,VRAM_BASE, PTE_W | PTE_R ); //
+    map(pgtbl,UART_BASE,UART_BASE, PTE_W | PTE_R ); //
+    map(pgtbl,TIMER_BASE,TIMER_BASE, PTE_W | PTE_R ); //
+}
+
+
+
+void putch_at(int x, int y, unsigned char ch) {
+	 VRAM[wk_mul(y,SCREEN_COLS) + x] = ch;
 }
 
 void trap_handler(void *epc, unsigned int cause) {
@@ -134,35 +124,39 @@ void trap_handler(void *epc, unsigned int cause) {
 	if (cause == 10 || cause == 15 || cause == 13) {
         t=info[0];
         for(int i=0;i<16;i++){
-            vputch_at(50+i,timercount,t[i]);
+            putch_at(i,timerflag,t[i]);
         }
         while(1);
 	} else if(cause == 0x80000007){
-        t=info[2];
-        for(int i=0;i<16;i++){
-            vputch_at(i,timercount,t[i]);
+        if (timerflag == 0){
+            t=info[1];
+            for(int i=0;i<16;i++){
+                VRAM[wk_mul(timerflag,SCREEN_COLS)+i]=t[i];
+            }
+//            enable_paging();
+        } else{
+            t=info[2];
+            for(int i=0;i<16;i++){
+                putch_at(i,timerflag,t[i]);
+            }
         }
-        timercount += 1;
     }
-    
+    timerflag += 1;
+    if(timerflag > SCREEN_ROWS){
+        timerflag = 1;
+    }
 }
 
-void clear_screen() {
-	int *vram = ((int *) VRAM_BASE);
-	for (int i = 0; i < 600; ++i) vram[i] = 0x20202020;
-}
 
 int main(){
-    clear_screen();
-    for(int i=0;i<24;i++){
-        putch_at(20+i,0,"printout before paging"[i]);
+	int *vram = ((int *) VRAM_BASE);
+    for (int i = 0; i < 600; ++i) vram[i] = 0x20202020;
+    for(int i=0;i<22;i++){
+        VRAM[20+i]="printout before paging"[i];
     }
     kvminit();
-    enable_paging();
-    int *vram = ((int *) VA_VRAM_BASE);
-	for (int i = 0; i < 600; ++i) vram[i] = 0x31313131;
     enable_interrupt();
-    *VA_TIMER_ENABLED = 1;
-    *VA_TIMER_LIMIT = INT_TIMER_LIMIT;
+    *TIMER_ENABLED = 1;
+    *TIMER_LIMIT = INT_TIMER_LIMIT;
     for(;;);
 }
